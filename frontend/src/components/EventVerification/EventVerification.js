6fee7af1f28483ca531c5bfd0b7b66f79c259058
Marcus Leong <mwleong25@gmail.com>
2022-08-28 14:06:11 -0500

    Cleanup + setup for event verification

--- frontend/src/components/EventVerification/EventVerification.js
+++ frontend/src/components/EventVerification/EventVerification.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 
-const EventVerfication = () => {
+const EventVerification = () => {
   const [event, setEvent] = useState({});
+  const [code, setCode] = useState();
+  const [location, setLocation] = useState({});
+
+  /**
+   *  Create input for code
+   *  Set code and location into event object
+   *  Pass event object as prop to PitchDetect component
+   */
 
   function getLocation() {
     if (navigator.geolocation) {
@@ -11,9 +19,18 @@ const EventVerfication = () => {
     }
   }
 
-  return (
-    <PitchDetect eventMetadata={event} />
+  function showPosition(position) {
+    setLocation(position);
+  }
+
+  getLocation();
+
+  return(
+    <div>
+      {!code && <div></div>}
+      {location && <div>{location}</div>}
+    </div>
   )
-} 
+}
 
 export default EventVerification;
